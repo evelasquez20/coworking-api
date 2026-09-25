5ca55746eb777eec7e837b3873e36d3a6d9f5b82
Engelberto Velasquez <engelvelasquez05@gmail.com>
2026-09-24 19:19:09 -0600

    Commit Inicial

---
+++ src/main/java/com/coworking/api/CoworkingApiApplication.java
@@ -0,0 +1,13 @@
+package com.coworking.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoworkingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoworkingApiApplication.class, args);
+	}
+
+}
