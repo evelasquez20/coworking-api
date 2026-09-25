62552ddbff941c5911a2435aab1d878150b8faee
Engelberto Velasquez <engelvelasquez05@gmail.com>
2026-09-25 14:20:04 -0600

    Configurar entorno local, docker-compose con postgresql y perfiles de aplicacion

--- src/main/java/com/coworking/api/CoworkingApiApplication.java
+++ src/main/java/com/coworking/api/CoworkingApiApplication.java
@@ -2,12 +2,13 @@ package com.coworking.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CoworkingApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoworkingApiApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/coworking/api/config/security/AppProperties.java
@@ -0,0 +1,37 @@
+package com.coworking.api.config.security;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "app")
+public class AppProperties {
+
+    private Jwt jwt = new Jwt();
+    private Cors cors = new Cors();
+    private PaymentGateway paymentGateway = new PaymentGateway();
+
+    @Getter
+    @Setter
+    public static class Jwt {
+        private String secret;
+        private long expirationMs;
+    }
+
+    @Getter
+    @Setter
+    public static class Cors {
+        private String allowedOrigins;
+    }
+
+    @Getter
+    @Setter
+    public static class PaymentGateway {
+        private String url;
+    }
+
+}
